b3153e594e707002a992e250340fc24b37bdf6e7
liuzhihao1 <liuzhihao1@yy.com>
2022-02-17 16:57:42 +0800

    feat: 弹窗类型

--- src/pages/Editor/ExecutionConfigs/ShowModalConfig.tsx
+++ src/pages/Editor/ExecutionConfigs/ShowModalConfig.tsx
@@ -1,4 +1,4 @@
-import { Form, Input } from "antd";
+import { Form, Input, Select, Tag } from "antd";
 import React, { FC } from "react";
 
 interface Props {
@@ -13,6 +13,25 @@ export const ShowModalConfig: FC<Props> = ({ name }) => {
         rules={[{ required: true }]}
       >
         <Input placeholder="请填写" />
+      </Form.Item>
+      <Form.Item label="类型" name={`${name}.type`}>
+        <Select defaultValue="info">
+          <Select.Option value="info">
+            <Tag color="default">普通 info</Tag>
+          </Select.Option>
+          <Select.Option value="success">
+            <Tag color="success">成功 success</Tag>
+          </Select.Option>
+          <Select.Option value="error">
+            <Tag color="error">错误 error</Tag>
+          </Select.Option>
+          <Select.Option value="warn">
+            <Tag color="warning">警告 warn</Tag>
+          </Select.Option>
+          <Select.Option value="confirm">
+            <Tag color="warning">确认 confirm</Tag>(带取消按钮)
+          </Select.Option>
+        </Select>
       </Form.Item>
       <Form.Item
         label="内容"
